a6f0d0226ef4f7acc5ecbc7bc8593b4e5d02f449
dhurvthiside <dhruvchauhan6488@gmail.com>
2025-08-06 23:43:37 +0530

    Analytics added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Analytics } from '@vercel/analytics/react';
 import Navbar from './components/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
@@ -37,6 +38,7 @@ const App = () => {
         <Route path='/management' element={<PropertyManagement />} />
       </Routes>
       <Footer />
+      <Analytics />
     </div>
   )
 }
